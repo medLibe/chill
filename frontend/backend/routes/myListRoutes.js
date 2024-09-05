461b0be0fce6b3f5232797c6c44897a543b7ec2e
Gerry Prasetiyo <65025419+medLibe@users.noreply.github.com>
2024-09-05 16:42:59 +0700

    Delete frontend/backend directory
    double directory

--- frontend/backend/routes/myListRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const myListController = require('../controllers/myListController')
-
-router.post('/', myListController.addToMyList)
-router.get('/:user_id', myListController.getAllMyListByUserId)
-router.delete('/:id/:type', myListController.removeFromMyList)
-
-module.exports = router
